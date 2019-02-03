76638ef1a740675a28588e41a13a4d05f4c1f940
DeividZavala <crusedmoss@gmail.com>
2019-02-03 11:09:57 -0600

    otro approach para lab-dom :octocat:

--- semana-3/lab-dom-ironhack-cart/starter-code/js/index.js
+++ semana-3/lab-dom-ironhack-cart/starter-code/js/index.js
@@ -1,9 +1,9 @@
 var items = document.getElementsByClassName("unit_price");
-console.log(items);
-var inputs = document.getElementsByTagName("input");
-console.log(inputs)
-var total = 0;
 
+var inputs = document.getElementsByTagName("input");
+
+// otro approach
+var containers = document.getElementsByClassName("item-container");
 
 function deleteItem(e){
 
@@ -18,13 +18,26 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var total = 0;
+
+  //Otro approach
+  for(var r = 0; r < containers.length; r++){
+    let u = parseInt(containers[r].childNodes[3].firstElementChild.innerText.slice(1));
+    let qty = parseInt(containers[r].childNodes[5].childNodes[3].value)
+    // agregando total por producto
+    containers[r].childNodes[7].firstElementChild.innerText = `$${u * qty}`
+  }
+
+
+
   for(var i = 0; i < items.length; i++){
     let u = parseInt(items[i].innerText.slice(1));
     let qty = parseInt(inputs[i].value);
+    // en este código falta editar el total por producto
     total = total + ( u * qty)
-    console.log(u, qty)
   }
-  console.log(total);
+  // agregando el total al carrito
+  document.querySelector("h2").innerText = `Total: $${total.toFixed(2)}`
 }
 
 function createQuantityInput(){
@@ -53,11 +66,11 @@ function createNewItem(){
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+  //var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
