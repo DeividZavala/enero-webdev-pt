8c635395d9b42abeac8597619a555e35d1488002
DeividZavala <crusedmoss@gmail.com>
2019-03-23 16:43:39 -0600

    list of books

---
+++ semana-10/sabado/segunda-app/routes/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/Book");
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/books", (req, res) => {
+  //const { page = 0 } = req.query;
+  //let limit = 5;
+  //let skip = page * limit;
+  Book.find()
+    //.limit(limit)
+    //.skip(skip)
+    .then(books => {
+      res.render("books", { books });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/detail/:id", (req, res) => {
+  const { id } = req.params;
+  Book.findById(id).then(book => {
+    res.render("detail", book);
+  });
+});
+
+module.exports = router;
