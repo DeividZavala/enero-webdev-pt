f529b723620923b0822d9e3b02d3458269cc3975
DeividZavala <crusedmoss@gmail.com>
2019-03-30 13:02:04 -0600

    finished

--- semana-11/sabado/passport-auth/routes/auth.js
+++ semana-11/sabado/passport-auth/routes/auth.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const passport = require("passport");
 
 router.get("/login", (req, res) => {
   res.render("auth-form", { login: true });
 });
 
-router.post("/login", (req, res) => {});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/private",
+    failureRedirect: "/auth/login",
+    failureFlash: "Email o contraseña invalidos"
+  }),
+  (req, res) => {}
+);
 
 router.get("/register", (req, res) => {
   res.render("auth-form");
@@ -18,8 +28,15 @@ router.post("/register", (req, res) => {
     return res.render("auth-form", {
       err: "Las contras no son las mismas perro"
     });
+
+  User.register({ email }, password).then(user => {
+    res.redirect("/auth/login");
+  });
 });
 
-router.post("/logout", (req, res) => {});
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/auth/login");
+});
 
 module.exports = router;
--- semana-11/sabado/passport-auth/routes/index.js
+++ semana-11/sabado/passport-auth/routes/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const isAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/auth/login");
+};
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  console.log("logged", req.user);
+  res.render("index");
+});
+
+router.get("/private", isAuth, (req, res) => {
+  let { user } = req;
+  res.render("private", { user });
 });
 
 module.exports = router;
