a59c7f876ddb5cc1d4452c1928df92cb79db8be3
DeividZavala <crusedmoss@gmail.com>
2019-03-30 12:04:40 -0600

    auth-form

--- semana-11/sabado/passport-auth/routes/auth.js
+++ semana-11/sabado/passport-auth/routes/auth.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/login", (req, res) => {});
-router.get("/register", (req, res) => {});
+router.get("/login", (req, res) => {
+  res.render("auth-form", { login: true });
+});
+
+router.post("/login", (req, res) => {});
+
+router.get("/register", (req, res) => {
+  res.render("auth-form");
+});
+
+router.post("/register", (req, res) => {});
+
 router.post("/logout", (req, res) => {});
 
 module.exports = router;
