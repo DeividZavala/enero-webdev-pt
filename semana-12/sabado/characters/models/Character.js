602df8af719da609c88c10617fb9553f6afddd8d
DeividZavala <crusedmoss@gmail.com>
2019-04-06 10:47:00 -0600

    basic config

---
+++ semana-12/sabado/characters/models/Character.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const characterSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: "Debes mandar un nombre"
+    },
+    age: Number,
+    birthday: Date,
+    gender: {
+      type: String,
+      enum: ["male", "female"]
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Character", characterSchema);
---
+++ semana-12/sabado/characters/views/characters.hbs
@@ -0,0 +1,1 @@
+<h1>characters</h1>
