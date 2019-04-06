465cddafd1db4c17c7f5d9256138e127b70dddfa
DeividZavala <crusedmoss@gmail.com>
2019-04-06 11:18:01 -0600

    api

---
+++ semana-12/sabado/characters/perro.js
@@ -0,0 +1,1 @@
+epale epale
--- semana-12/sabado/characters/routes/characters.js
+++ semana-12/sabado/characters/routes/characters.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Characters = require("../models/Character");
 
 router.get("/", (req, res) => {
   res.render("characters");
+});
+
+router.get("/info", (req, res) => {
+  Characters.find()
+    .then(characters => {
+      res.status(200).json({ characters });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
