0c7b5c88b79bd3f44358efb0d7456725465a061e
DeividZavala <crusedmoss@gmail.com>
2019-04-09 21:05:23 -0500

    mandando carritos

--- semana-13/martes/multer-project/routes/cars.js
+++ semana-13/martes/multer-project/routes/cars.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const Car = require("../models/Car");
+const multer = require("multer");
+const upload = multer({
+  dest: `./public/images`
+});
+
+router.get("/", (req, res) => {
+  Car.find().then(cars => {
+    res.render("list", { cars });
+  });
+});
 
 router.get("/new", (req, res) => {
   res.render("form");
+});
+
+router.post("/new", upload.single("image"), (req, res) => {
+  req.body.image = `/images/${req.file.filename}`;
+  req.body.new = req.body.new === "on" ? true : false;
+
+  Car.create(req.body).then(car => {
+    res.json(car);
+  });
 });
 
 module.exports = router;
