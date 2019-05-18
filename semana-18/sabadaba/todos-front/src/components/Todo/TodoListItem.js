d1cdf11bc187d599fb8cd2fe9954fb27077c78ac
DeividZavala <crusedmoss@gmail.com>
2019-05-18 13:20:19 -0500

    card modifications

--- semana-18/sabadaba/todos-front/src/components/Todo/TodoListItem.js
+++ semana-18/sabadaba/todos-front/src/components/Todo/TodoListItem.js
@@ -13,6 +13,18 @@ const TodoListItem = ({body, priority, deleteItem}) => (
         onClick={deleteItem}
         type="button" 
         uk-close="true"></button>
+        <div className="uk-inline">
+            <button className="uk-button" type="button">
+                <span uk-icon="icon: chevron-down"></span>
+            </button>
+            <div uk-dropdown="true">
+                <ul className="uk-nav uk-dropdown-nav">
+                    <li><a href="#">Editar <span uk-icon="icon: pencil"></span></a></li>
+                    <li className="uk-nav-divider"></li>
+                    <li><a href="#">Eliminar <span uk-icon="icon: trash"></span></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 );
 
